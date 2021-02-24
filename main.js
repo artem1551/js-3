71ec86d198fd81152e956b0424cdd369164662d5
Artem Vovk <artem.vovk.m@gmail.com>
2021-02-24 22:28:36 +0200

    server created

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET', '/test.txt');
-xhr.send()
+const btnSend = document.querySelector('.sendForm');
+const num = /[0-9]{,3}/;
+const letter = /[A-Za-zА-Яа-яЁё]/;
+
+function request(method, url, body){
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange', () => {
+        if (xhr.readyState == 4) {
+            return
+        };
+    });
+    
+    xhr.open(method, url, true);
+    xhr.send(JSON.stringify(body));
+};
+
+btnSend.addEventListener('click', event => {
+    event.preventDefault
+    const name = document.querySelector('.name')
+    const age = document.querySelector('.age')
+    body = { 
+        name: name.value,
+        age:  age.value,
+    };
+    
+    request('POST', '/registration', body);
+});
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const static = require('node-static');
 const Server = static.Server;
 const file = new Server('.');
-const port = 1111;
+const port = 1234;
 
 const app = http.createServer(function(req, res) {
   file.serve(req, res);
